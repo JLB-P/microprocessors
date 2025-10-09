4ec7724f99fee28b9d8ae52126ee79133f1563f0
jlb <JLB-P@users.noreply.github.com>
2025-10-09 12:19:08 -0600

    Add I2C initialization and control to main
    Included i2c.h in main.c and added calls to init_i2c(), start_i2c(), and stop_i2c() in main(). Updated project and build files to reflect the new I2C dependencies and function usage.

--- c-/main.c
+++ c-/main.c
@@ -1,5 +1,6 @@
 #include "gpio.h"
 #include "ext_in.h"
+#include "i2c.h"
 
 int main()
 {
@@ -7,6 +8,10 @@ int main()
 	//init_portc();
 	init_ports();
 	init_exti();
+	init_i2c();
+	start_i2c();
+	/*..*/
+	stop_i2c();
 	while(1){
 
 	}
